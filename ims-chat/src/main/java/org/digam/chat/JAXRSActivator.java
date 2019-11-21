17096d6c4cb7ab322eb15e4840db33644c28e20b
Elsadiq <you@example.com>
2019-11-21 16:24:44 +0100

    Initial commit of ims-chat

---
+++ ims-chat/src/main/java/org/digam/chat/JAXRSActivator.java
@@ -0,0 +1,9 @@
+package org.digam.chat;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSActivator extends Application {
+
+}
